2669f64ff18c949419021a0d8874dba30421be8a
raulsync <rahul.aanand086@gmail.com>
2024-10-28 13:31:41 +0530

    playing-with-multiple-routes

--- src/app.js
+++ src/app.js
@@ -4,10 +4,14 @@ const app = express(); //instance of express
 
 const port = 7777;
 
-//order of routes matter when we /hello/2 => it will go to /hello routes because first it check /hello string exist or not.
-
-//if we use / as a path then after that if anything comes it will always go to / routes
-
+/*
+order of routes matter when we /hello/2 => it will go to /hello routes
+because first it check /hello string exist or not.
+*/
+/*
+***if we use / as a path then after that if anything comes
+ it will always go to / routes
+ /*
 // app.use("/", (req, res) => {
 //   res.send("Hello from namaste dashboard");
 // });
@@ -26,8 +30,8 @@ const port = 7777;
 
 /*
 
- * order of execution matter here if we test any route it start from here if path matched then it doesn't go to next route
-
+* order of execution matter here if we test any route it start from here
+ if path matched then it doesn't go to next route
 
  app.use("/user", (req, res) => {
    res.send("Testing app.use ! ");
@@ -36,7 +40,9 @@ const port = 7777;
 */
 
 /*
-  ? => optional + => (repeatitive same char)  bc*cd=> bcafjahfhfcd => it means path starts with bc and in between we put any character and it ends with cd
+  ? => optional + => (repeatitive same char)
+  bc*cd=> bcafjahfhfcd => it means path starts with bc and in
+   between we put any character and it ends with
   /.*baby$ / => it will match all path that ends with baby
   we can also use regex instead of string in route path
 */
@@ -49,19 +55,74 @@ const port = 7777;
 // });
 
 /*
-  - Playing with query param and dynamic route param
+  -Playing with query param and dynamic route param
   -req.query to get the urlquery param that is passed from client side
   -req.params to get the dynamic data that pass from url as a params
 */
 
-app.get("/user/:userId/:password", (req, res) => {
-  console.log(req.query);
-  console.log(req.params);
-  res.send({
-    userName: "Rahul Anand",
-    emailId: "rahul123@gmail.com",
-  });
-});
+// app.get("/user/:userId/:password", (req, res) => {
+//   console.log(req.query);
+//   console.log(req.params);
+//   res.send({
+//     userName: "Rahul Anand",
+//     emailId: "rahul123@gmail.com",
+//   });
+// });
+
+/*
+  we can also wrap route handler in array it will not gives any error
+*/
+
+app.use(
+  "/user",
+  [
+    (req, res, next) => {
+      console.log("Response !!!");
+      /*
+  *** if we do not sent the response from server
+  and if url get hit by user or in postman
+  then it run in loop it will
+  resolve the request BUT IT TAKES TIMES
+
+
+  --- we can also make multiple route handler inside app.use or app.get/put/post/delete---
+  */
+      // res.send("1st Response");
+
+      /*
+ this route never goes to the next route because the response
+ is send to the client
+
+ so here we use next() method that is provided by expressJs as a
+ third arguement in route handler to handle this type of scenario
+ */
+      next();
+    },
+    (req, res, next) => {
+      console.log("2nd Response");
+      // res.send("2nd Response");
+      next();
+    },
+    (req, res, next) => {
+      console.log("3rd Response");
+
+      // res.send("3rd Response !! ");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("4th Response");
+    // res.send("4th Response !! ");
+    //if we do not provide next route handler after next then it gives error bcs express expect next route handle when we use next method
+    next();
+  },
+  (req, res, next) => {
+    console.log("5th Response");
+    // if we do not send back the response it will run in loop
+    res.send("5th Response !! ");
+    // next();
+  },
+);
 
 //testing get call
 // app.get("/user", (req, res) => {
@@ -71,18 +132,18 @@ app.get("/user/:userId/:password", (req, res) => {
 //   });
 // });
 
-app.post("/user", (req, res) => {
-  console.log("Data save to database successfully");
-  res.send("Data save to database");
-});
+// app.post("/user", (req, res) => {
+//   console.log("Data save to database successfully");
+//   res.send("Data save to database");
+// });
 
-app.delete("/user", (req, res) => {
-  res.send("user data deleted from database successfully !");
-});
+// app.delete("/user", (req, res) => {
+//   res.send("user data deleted from database successfully !");
+// });
 
-app.use("/test", (req, res) => {
-  res.send("Hello from test route");
-});
+// app.use("/test", (req, res) => {
+//   res.send("Hello from test route");
+// });
 
 app.listen(port, () => {
   console.log("server is listening on port 7777");
