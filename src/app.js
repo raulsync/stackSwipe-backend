e1aca10c0213bc5fe11a63d106c091bec8619e66
raulsync <rahul.aanand086@gmail.com>
2024-10-29 21:13:39 +0530

    play_with_db

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 const express = require("express")
 const app = express() //instance of express
 const { dbConnect } = require("./config/database")
+const User = require("./models/user")
 const port = 7777
+
+app.post("/signup", async (req, res) => {
+  const user = {
+    firstName: "Rahul",
+    lastName: "Anand",
+    emailId: "rahul@anand.com",
+    password: "rahul@123",
+  }
+  try {
+    const userData = new User(user)
+    await userData.save()
+    res.send("user data saved successfully")
+  } catch (error) {
+    console.error("Something went wrong", error.message)
+    res.status(400).send("some error occured")
+  }
+})
 
 dbConnect()
   .then(() => {
