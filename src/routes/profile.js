bf286cfc9de47b301b6dc3d03c5a3a4f264c7d22
raulsync <rahul.aanand086@gmail.com>
2024-11-09 03:26:10 +0530

    updatePassword_api_completed

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfile } = require("../utils/validate");
 const profileRouter = express.Router();
 
-//Profile Router
+//Profile/view Router
 profileRouter.get("/profile/view", userAuth, (req, res) => {
   try {
     // console.log(req);
@@ -16,6 +17,8 @@ profileRouter.get("/profile/view", userAuth, (req, res) => {
   }
 });
 
+//update Profile
+
 profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
   try {
     //validate req.body
@@ -23,19 +26,60 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
       throw new Error("please only update the appropriate fields ");
     }
     const loggedInUser = req.user;
-    console.log(loggedInUser);
 
     Object.keys(req.body).forEach(
       (fields) => (loggedInUser[fields] = req.body[fields])
     );
     await loggedInUser.save();
-    console.log(loggedInUser);
     res.json({
       message: `${loggedInUser.firstName} , your profile is updated successfully`,
       data: [loggedInUser],
     });
   } catch (error) {
     res.status(400).send("Error : " + error.message);
+  }
+});
+
+//update Password
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    validateUpdatePassword(req);
+    //get current password
+    // const currentPassword = user.password;
+    const { currentPassword, newPassword } = req.body;
+    console.log(currentPassword, newPassword);
+
+    if (!currentPassword || !newPassword) {
+      throw new Error("All password fields are required");
+    }
+    if (currentPassword === newPassword) {
+      throw new Error("your new password is same as current password");
+    }
+
+    //check password present in db
+    const isValidPassword = await bcrypt.compare(
+      currentPassword,
+      user.password
+    );
+
+    if (!isValidPassword) {
+      throw new Error("password is not valid");
+    }
+
+    //validate Password
+    //----------------------
+
+    const hashPassowrd = await bcrypt.hash(newPassword, 10);
+    user.password = hashPassowrd;
+    await user.save();
+    res.send("password updated successfully");
+  } catch (error) {
+    res.status(400).send("Error :" + error.message);
   }
 });
 
